d36c87044af868fbf0e223c99dbcfefda060357d
Bill Feng <bill.zijing.feng@gmail.com>
2015-10-28 13:37:28 -0400

    reorganizing files

--- src/main/java/autotradingsim/experiment/Experiment.java
+++
@@ -1,9 +0,0 @@
-package autotradingsim.experiment;
-
-/**
- * Created by Asher on 2015-10-25.
- *
- * Experiments apply Strategies to particular stocks, for a set of time periods.
- */
-public class Experiment {
-}
--- src/main/java/autotradingsim/strategy/Action.java
+++
@@ -1,8 +0,0 @@
-package autotradingsim.strategy;
-
-/**
- * Created by Asher on 2015-10-26.
- */
-public enum Action {
-    BUY, SELL, SHORT, COVER
-}
