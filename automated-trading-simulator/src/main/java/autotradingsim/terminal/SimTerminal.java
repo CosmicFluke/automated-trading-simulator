7e3bb2b5f9b14068462c5f3d017f380aaf9a935b
xshirl1027 <xshirl1027@gmail.com>
2015-10-30 18:37:49 -0400

    adding architecture outline made by asher

--- automated-trading-simulator/src/main/java/autotradingsim/terminal/SimTerminal.java
+++ automated-trading-simulator/src/main/java/autotradingsim/terminal/SimTerminal.java
@@ -28,49 +28,51 @@ public class SimTerminal {
 		String input ="";
 		br = new BufferedReader(new InputStreamReader(System.in));
 		while(input!=null){
+			output="";
+			if(runningExperiment!=""){
+				System.out.print(runningExperiment+" ");
+			}
+			System.out.print("$>");
+			
 			try {
-				output="";
-				if(runningExperiment!="")System.out.print(runningExperiment+" ");
-				System.out.print("$>");
-				input =br.readLine();
-				String[] args = input.split("\\s+");
-				switch(args[0].toLowerCase()){
-					case "h":
-					case "help" :{
-						output="You asked for help!";
-					}break;
-					case "exit" :{
-						runningExperiment="";
-					}break;
-					case "modify": { //if file args[1] not found in local dir
-									//create new file named args[1]
-						if(args.length!=2){ //should be modify [experiment name]
-							output="Incorrect number of arguments!";
-						}else{
-							runningExperiment=args[1];
-						}
-					}break;
-					case "run": {
-						if(runningExperiment.isEmpty()){
-							output="You have no experiments open right now.";
-						}else{
-							output="So you want to run " +runningExperiment;
-						}
-					}break;
-					case "": output="";
-					break;
-					default :{
-						output="That is not a command.";
-					}
-				}
-				if(!output.isEmpty()){
-					System.out.print(""+output+" \n");
-				}
-				
+				input =br.readLine();	
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			
+			String[] args = input.split("\\s+");
+			switch(args[0].toLowerCase()){
+				case "h":
+				case "help" :{
+					output="You asked for help!";
+				}break;
+				case "exit" :{
+					runningExperiment="";
+				}break;
+				case "modify": { //if file args[1] not found in local dir
+								//create new file named args[1]
+					if(args.length!=2){ //should be modify [experiment name]
+						output="Incorrect number of arguments!";
+					}else{
+						runningExperiment=args[1];
+					}
+				}break;
+				case "run": {
+					if(runningExperiment.isEmpty()){
+						output="You have no experiments open right now.";
+					}else{
+						output="So you want to run " +runningExperiment;
+					}
+				}break;
+				case "": output="";
+				break;
+				default :{
+					output="That is not a command.";
+				}
+			}
+			if(!output.isEmpty()){
+				System.out.print(""+output+" \n");
+			}
 		}
 	}
 	public static void main(String[] args){
