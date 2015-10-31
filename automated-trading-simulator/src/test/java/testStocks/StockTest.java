0588b8f63bf6766d42a2ca7b417fa3338da67d15
Ujash Joshi <ujash.joshi@utoronto.ca>
2015-10-31 14:33:08 -0400

    * Added testStocks package with empty JUnit tests.
    * Moved APPL.csv to test/resources/DATA/STOCKS
    
    * Added symbol list to test/resources/DATA

---
+++ automated-trading-simulator/src/test/java/testStocks/StockTest.java
@@ -0,0 +1,14 @@
+package testStocks;
+
+import org.junit.Before;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class StockTest extends TestCase{
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+}
