262c9d9a80017decf6a78f05f249425e14ad74d8
Bill Feng <bill.zijing.feng@gmail.com>
2015-11-01 12:25:42 -0500

    modifying indicators

---
+++ automated-trading-simulator/src/main/java/autotradingsim/strategy/indicators/SimpleMovingAverage.java
@@ -0,0 +1,25 @@
+package autotradingsim.strategy.indicators;
+
+import autotradingsim.stocks.StockDay;
+import autotradingsim.strategy.Indicator;
+
+/**
+ * Created by Bill Feng on 15-10-29.
+ *
+ * Simple Moving Average
+ *      Calculated by taking the average of the past days, the length of which can be specified.
+ *      When updating, add the new data and take the new average.
+ */
+public class SimpleMovingAverage extends Indicator{
+
+    private int days;
+
+    public SimpleMovingAverage(double init, int days){
+        super("Simple Moving Average", "The Simple Moving Average is calculated by summing the closing prices of the security for a period of time and then dividing this total by the number of time periods. Sometimes called an arithmetic moving average, the SimpleMovingAverage is basically the average stock price over time. As a trend develops, the moving average will slope in the direction of the trend, showing the trend direction and some indication of its strength based on the slope steepness.\n Note that since the Simple Moving Average gives equal weight to each daily price, the longer time period studied suggests the greater smoothing out of recent market volatility. Long-term moving averages smooth out all the minor fluctuations showing only longer-term trends. Shorter-term moving averages show shorter-term trends but at the expense of the long-term ones.", init);
+        this.days = days;
+    }
+
+    public void update(StockDay data){
+        setValue((this.getValue() * (days - 1) + data.getClose()) / days);
+    }
+}
