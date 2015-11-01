825918182dc166259fbfd5e3921259aa48d750d6
xshirl1027 <xshirl1027@gmail.com>
2015-11-01 16:35:10 -0500

    o Please enter the commit message for your changes. Lines starting

--- automated-trading-simulator/src/main/java/autotradingsim/engine/TradingEngine.java
+++ automated-trading-simulator/src/main/java/autotradingsim/engine/TradingEngine.java
@@ -3,11 +3,13 @@ package autotradingsim.engine;
 import java.util.ArrayList;
 
 import autotradingsim.experiment.Experiment;
-
+import autotradingsim.strategy.Strategy;
+import autotradingsim.strategy.Condition;
+import autotradingsim.strategy.Action;
 public class TradingEngine {
 
-	ArrayList<Experiment> experiments;
-	
+	static ArrayList<Experiment> experiments;
+	static ArrayList<Strategy> strategies;
 	public TradingEngine(){
 		defineDefaults();
 	}
@@ -17,18 +19,50 @@ public class TradingEngine {
 		defaultExp.addStock("APPL");
 	}
 
-	public String viewExperiment(String experimentName){
+	public static String viewExperiment(String experimentName){
 		Experiment query= findExperiment(experimentName);
 		if(query == null)
 			query = findExperiment("default");
 		return query.getName();
 	}
+	public static void viewStrategy(String[] args) {
+		if(args.length==2){
+			//display strategy with name arg[1]
+		}else{
+			//display a list of strategies
+		}
+	}
 
-	private Experiment findExperiment(String name) {
+	private static Experiment findExperiment(String name) {
 		for(Experiment curr: experiments){
 			if(curr.getName().equals(name))
 				return curr;
 		}
 		return null;
+	}
+
+	public static void addStrategy(String currentExperiment, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void addExpTime(String currentExperiment, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void viewStrategy(String currentExperiment, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void addCond(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void addAction(String[] args) {
+		// TODO Auto-generated method stub
+		
 	}
 }
