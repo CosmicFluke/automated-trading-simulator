f577afe24fd753ec3d18b0ee2200b7674fb6b023
Asher Minden-Webb <amindenwebb@gmail.com>
2015-11-01 20:58:10 -0500

    Added interfaces for Strategy and StrategyTester API

--- automated-trading-simulator/src/main/java/autotradingsim/strategy/Strategy.java
+++ automated-trading-simulator/src/main/java/autotradingsim/strategy/Strategy.java
@@ -3,6 +3,6 @@ package autotradingsim.strategy;
 /**
  * Created by Asher on 2015-10-30.
  */
-public abstract class Strategy implements IRuleSet {
+public abstract class Strategy implements IStrategy {
 
 }
