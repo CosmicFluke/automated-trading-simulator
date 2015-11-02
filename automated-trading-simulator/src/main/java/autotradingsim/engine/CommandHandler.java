a9cef5b31f386661527616d9443c90e122802bad
xshirl1027 <xshirl1027@gmail.com>
2015-11-02 05:59:38 -0500

    sdasdasdnl

--- automated-trading-simulator/src/main/java/autotradingsim/engine/CommandHandler.java
+++ automated-trading-simulator/src/main/java/autotradingsim/engine/CommandHandler.java
@@ -1,26 +1,48 @@
 package autotradingsim.engine;
 import autotradingsim.strategy.*;
+
+import java.util.Set;
+
 import autotradingsim.experiment.*;
 public class CommandHandler {
 
 	public CommandHandler() {
 		// TODO Auto-generated constructor stub
 	}
-
-	protected void createStrategy(String name){
-		//get list of all conditions
-		//apply condition to rule
+	public TradingApplication appEngine=TradingApplication.getInstance();
+	
+	protected void createStrategy(){
+		appEngine.saveStrategy(null);
 	}
+	
 	public void addStrategy(String exp, String strat){
 		
 	}
 
-	public static void viewStrategy(String[] args) {
+	public void viewStrategy(String stratname) {
 		// TODO Auto-generated method stub
+		if(stratname.isEmpty()){
+			System.out.println("Strategy List");
+		}else{
+			IStrategy strat=null;
+			Set<RuleID> rules;
+			if((strat=appEngine.getStrategy(stratname))!=null){
+				rules=strat.getRules();
+				System.out.println(strat.getName());
+				for (RuleID r:rules){
+					System.out.println(strat.getRuleName(r));
+					System.out.println(strat.getRuleDescription(r));
+					System.out.println(strat.getRuleSummary(r));
+				}
+				
+				
+			}
+			
+		}
 		
 	}
 
-	public static void viewExperiment(String string) {
+	protected static void viewExperiment(String string) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -32,6 +54,14 @@ public class CommandHandler {
 
 	public static void addtimeset(String currentExperiment, String string, String string2) {
 		// TODO Auto-generated method stub
+		
+	}
+
+	public void createDefaultStrategy() {
+		// TODO Auto-generated method stub
+		SimpleStrategy newstrat=new SimpleStrategy();
+		appEngine.saveStrategy(newstrat);
+		System.out.println("Default strategy "+appEngine.getStrategy(newstrat.getName()).getName()+" created");
 		
 	}
 	
