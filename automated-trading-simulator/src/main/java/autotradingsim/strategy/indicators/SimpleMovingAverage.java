151679835da707ae9320d3c3e4f294b1d4b8c145
Bill Feng <bill.zijing.feng@gmail.com>
2015-11-02 04:13:57 -0500

    Correcting SimpleMovingAverage

--- automated-trading-simulator/src/main/java/autotradingsim/strategy/indicators/SimpleMovingAverage.java
+++ automated-trading-simulator/src/main/java/autotradingsim/strategy/indicators/SimpleMovingAverage.java
@@ -17,18 +17,26 @@ import java.util.function.Function;
 public class SimpleMovingAverage extends Indicator{
 
     private int days;
-    private double init;
+    private BigDecimal value;
     private static final String name = "Simple Moving Average";
     private static final String description = "The Simple Moving Average is calculated by summing the closing prices of the security for a period of time and then dividing this total by the number of time periods. Sometimes called an arithmetic moving average, the SimpleMovingAverage is basically the average stock price over time. As a trend develops, the moving average will slope in the direction of the trend, showing the trend direction and some indication of its strength based on the slope steepness.\n Note that since the Simple Moving Average gives equal weight to each daily price, the longer time period studied suggests the greater smoothing out of recent market volatility. Long-term moving averages smooth out all the minor fluctuations showing only longer-term trends. Shorter-term moving averages show shorter-term trends but at the expense of the long-term ones.";
 
-    public SimpleMovingAverage(double init, int days){
+    public SimpleMovingAverage(BigDecimal init, int days){
         super(name, description);
-        this.init = init;
+        this.value = init;
         this.days = days;
     }
 
     public void update(StockDay data){
-        // setValue((this.getValue() * (days - 1) + data.getClose()) / days);
+        setValue((this.getValue().multiply(new BigDecimal(days - 1)).add(data.getValue().divide(new BigDecimal(days)))));
+    }
+
+    public BigDecimal getValue(){
+        return this.value;
+    }
+
+    public void setValue(BigDecimal value){
+        this.value = value;
     }
 
     @Override
