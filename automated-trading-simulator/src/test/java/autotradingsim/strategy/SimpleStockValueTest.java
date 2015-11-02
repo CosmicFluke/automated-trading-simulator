3f958a9edde0db047dbd9d987114e2cf1c7b34aa
Asher Minden-Webb <amindenwebb@gmail.com>
2015-11-02 00:28:44 -0500

    Fixed SimpleStockValueTest with new JUnit

--- automated-trading-simulator/src/test/java/autotradingsim/strategy/SimpleStockValueTest.java
+++ automated-trading-simulator/src/test/java/autotradingsim/strategy/SimpleStockValueTest.java
@@ -2,19 +2,21 @@ package autotradingsim.strategy;
 
 import autotradingsim.stocks.IStock;
 import autotradingsim.stocks.StockLoader;
-import junit.framework.TestCase;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import static org.junit.Assert.*;
+
 /**
- * Created by Asher on 2015-11-01.
+ * Created by Asher on 2015-11-02.
  */
-public class SimpleStockValueTest extends TestCase {
+public class SimpleStockValueTest {
 
+    @Test
     public void testGetValue() throws Exception {
-
         StockLoader stockLoader = new StockLoader();
         IStock stock = stockLoader.fetchStock("AAPL");
         IMeasurement testObj = new SimpleStockValue();
