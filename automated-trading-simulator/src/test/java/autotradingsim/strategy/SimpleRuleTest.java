f535a8e3d8446c4a9a3225ae53d063e34852f5a2
Asher Minden-Webb <amindenwebb@gmail.com>
2015-11-05 19:03:10 -0500

    Added SimpleRuleTest and moved AppPlaceholderTest to new engine test package

---
+++ automated-trading-simulator/src/test/java/autotradingsim/strategy/SimpleRuleTest.java
@@ -0,0 +1,35 @@
+package autotradingsim.strategy;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Asher on 2015-10-30.
+ */
+public class SimpleRuleTest extends TestCase {
+
+    protected ICondition simpleCondition;
+    protected IAction simpleAction;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        simpleAction = new SimpleAction(IAction.ActionType.BUY, 2);
+        simpleCondition = null;
+
+    }
+
+    public void testGetRuleID() throws Exception {
+
+    }
+
+    public void testGetDecisionMaker() throws Exception {
+
+    }
+
+    public void testGetConditions() throws Exception {
+
+    }
+
+    public void testGetActions() throws Exception {
+
+    }
+}
