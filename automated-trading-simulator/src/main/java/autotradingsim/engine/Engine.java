ea4229d728a91cd5c39e5556ed2347e60a2e5583
xshirl1027 <xshirl1027@gmail.com>
2015-11-10 20:03:28 -0500

    added javadocs for experimentEngine

--- automated-trading-simulator/src/main/java/autotradingsim/engine/Engine.java
+++ automated-trading-simulator/src/main/java/autotradingsim/engine/Engine.java
@@ -11,83 +11,13 @@ import autotradingsim.strategy.simpleimpl.*;
 import autotradingsim.terminal.*;
 public class Engine {
 
-	public TradingApplication appEngine = TradingApplication.getInstance();
-	
-	protected void createStrategy(){
-		appEngine.saveStrategy(null);
-	}
-	
-	public void addStrategy(String exp, int stratid){
-		
-	}
-	
-	public void viewStrategy(String stratname) {
-		System.out.println("List of Strategies:");
-		IStrategy strat = null;
-		if((strat = appEngine.getStrategy(stratname)) != null){
-			System.out.println(strat.getName());
-			for (RuleID r : strat.getRules()){
-				System.out.println(strat.getRuleName(r));
-				System.out.println(strat.getRuleDescription(r));
-				System.out.println(strat.getRuleSummary(r));
-				//System.out.println(strat.getRuleDecisionMaker(r));
-			}
-		}
-	}
-	
-
-	protected static void viewExperiment(String string) {
-		// TODO Auto-generated method stub
+	public TradingApplication TradingSimApp = TradingApplication.getInstance();
+	private Engine(){
 		
 	}
 
-	public static void addtimeset(String currentExperiment, String string, String string2) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void createDefaultStrategy() {
-		// TODO Auto-generated method stub
-		SimpleStrategy newstrat = new SimpleStrategy();
-		appEngine.saveStrategy(newstrat);
-		System.out.println("Default strategy "+appEngine.getStrategy(newstrat.getName()).getName()+" created");
-		System.out.println("This is a read-only strategy. Returning to Main menu.");
-		
-	}
-	public void printconditions(){
-		System.out.println("Choose from the following conditions");
-		System.out.println("1. stock value is greater than x");
-		System.out.println("2. stock value is smaller than x");	
-	}
 	
-	public void printactions(){
-		System.out.println("Choose from the following actions");
-		System.out.println("1. buy y amount");
-		System.out.println("2. sell y amount");	
-	}
 	
-	/**
-	 * Adds a new simple strategy
-	 * 
-	 * @param stratname
-	 * @param cselection Must be either 1 or 2
-	 * @param cvalue 
-	 * @param aselection Must be either 1 or 2
-	 * @param avalue
-	 */
-	public IStrategy currentStrategy;
-	public void addNewSimpleStrategy(String stratname, int cselection, int cvalue, int aselection, int avalue) {
-		currentStrategy = 
-				new SimpleStrategy(
-						stratname, "",
-						(cselection == 1) ? Comparator.GT : Comparator.LT, new BigDecimal(cvalue),
-						(aselection == 1) ? IAction.ActionType.BUY : IAction.ActionType.SELL, avalue);
-		
-
-	}
-	public void saveCurrentStrategy(){
-		appEngine.saveStrategy((SimpleStrategy) currentStrategy);
-	}
-
+	
 	
 }
--- automated-trading-simulator/src/main/java/autotradingsim/engine/StrategyEngine.java
+++ automated-trading-simulator/src/main/java/autotradingsim/engine/StrategyEngine.java
@@ -18,12 +18,26 @@ public class StrategyEngine {
 		// TODO Auto-generated constructor stub
 		 appEngine = TradingApplication.getInstance();
 	}
+	/**
+	 * creates a default strategy and stores it in application
+	 * @return
+	 */
 	public IStrategy createDefaultStrategy() {
 		// TODO Auto-generated method stub
 		IStrategy newstrat = new Strategy();
 		appEngine.saveStrategy(newstrat);
 		return newstrat;
 	}
+	
+	/**
+	 * creates a simplestrategy, saves it to application and returns the strategy if saved properly
+	 * @param stratname
+	 * @param cselection
+	 * @param cvalue
+	 * @param aselection
+	 * @param avalue
+	 * @return strategy
+	 */
 
 	public IStrategy addNewSimpleStrategy(String stratname, int cselection, int cvalue, int aselection, int avalue) {
 		if(appEngine.saveStrategy(new SimpleStrategy(stratname, "",(cselection == 1) ? Comparator.GT : Comparator.LT,
@@ -31,10 +45,14 @@ public class StrategyEngine {
 			return appEngine.getStrategy(stratname.hashCode());
 		}else{
 			return null;
-		}
-		
+		}	
 	}
-
+	
+	/**
+	 * retrieve and return strategy from application with the given name
+	 * @param stratname
+	 * @return Strategy
+	 */
 	public IStrategy viewStrategy(String stratname) {
 		// TODO Auto-generated method stub
 		return(appEngine.getStrategy(stratname.hashCode()));
