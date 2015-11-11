2cde5e832c1056ea9202bb5cccc7c032f414b3af
Tomek-Stolarczyk <tomek.stolarczyk9@gmail.com>
2015-11-10 20:32:27 -0500

    adding interface for trading application and javadocs

--- automated-trading-simulator/src/main/java/autotradingsim/application/TradingApplication.java
+++ automated-trading-simulator/src/main/java/autotradingsim/application/TradingApplication.java
@@ -1,17 +1,5 @@
 package autotradingsim.application;
-import java.io.*;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
 import java.util.Iterator;
 
 import autotradingsim.experiment.*;
@@ -21,16 +9,34 @@ import autotradingsim.strategy.*;
 
 public class TradingApplication implements ITradingApplication {
 	private StockLoader loader;
-	public HashMap<Integer, IStrategy> strategies = new HashMap<Integer, IStrategy>();
-	public HashMap<Integer, IExperiment> experiments = new HashMap<Integer, IExperiment>();
-	private HashMap<String,IStock> stocks = new HashMap<String, IStock>();
+	private HashMap<Integer, IStrategy> strategies;
+	private HashMap<Integer, IExperiment> experiments;
+	private HashMap<String, IStock> stocks;
 
 	private static TradingApplication instance = null;
 	
-	protected TradingApplication() {
+	/**
+	 * Create a new Application, instantiate all objects.
+	 * Later on, look for a previous application object and load from it
+	 */
+	private TradingApplication() {
+		if(instance != null)
+			return;
+		
 		this.loader = new StockLoader();
+		this.strategies = new HashMap<Integer, IStrategy>();
+		this.experiments = new HashMap<Integer, IExperiment>();
+		this.stocks = new HashMap<String, IStock>();
+		
+		instance = this;
 	}
 	
+	/**
+	 * return the only instance of trading application
+	 * If none found at time of invokation, create it
+	 * 
+	 * @return trading application instance.
+	 */
 	public static TradingApplication getInstance(){
 		if (instance==null){
 			instance=new TradingApplication();
@@ -38,11 +44,18 @@ public class TradingApplication implements ITradingApplication {
 		return instance;
 	}
 	
-	/* (non-Javadoc)
-	 * @see autotradingsim.application.ITradingApplication#saveExperiment(autotradingsim.experiment.IExperiment)
+	/**
+	 * Add an experiment by name into the application
+	 * Name given and name found in experiment don't need
+	 * to match, but after being loaded, must use name found
+	 * in IExperiment class
+	 * 
+	 * @param experimentName name under which to store experiment
+	 * @param experiment Experiment object which will be stored
+	 * @return true if experiment added into Application successfully
 	 */
 	@Override
-	public boolean saveExperiment(IExperiment experiment){
+	public boolean setExperiment(String experimentName, IExperiment experiment){
 		if(experiments.containsKey(experiment.getName())){
 			return false;
 		}else{
@@ -51,120 +64,60 @@ public class TradingApplication implements ITradingApplication {
 		}
 	}
 	
-	/* (non-Javadoc)
-	 * @see autotradingsim.application.ITradingApplication#getExperiment(int)
+
+	/**
+	 * Return experiment object associated with given name
+	 * 
+	 * @param experimentName ID associated with experiment
+	 * @return experiment object found with ID. Null if no experiment by name found
 	 */
 	@Override
-	public Experiment getExperiment(int expID){
-		return (Experiment) experiments.get(expID);
+	public IExperiment getExperiment(String experimentName){
+		return null; //(Experiment) experiments.get(expID);
+	}
+
+	/**
+	 * Add a strategy by name into the application
+	 * StrategyName should match with name found under newStrat object
+	 * 
+	 * @param StrategyName name under to which to store experiment
+	 * @param newStrat IStrategy object which is to be added to application
+	 * @return true if strategy added successfully into application
+	 */
+	@Override
+	public boolean setStrategy(String stratName, IStrategy strat){
+		return false; //strategies.get(stratid);
 	}
 	
-	/* (non-Javadoc)
-	 * @see autotradingsim.application.ITradingApplication#displayResults(java.lang.String)
+
+	/**
+	 * Retrieves a strategy by it's given name
+	 * 
+	 * @param stratname strategy name which was used to store strategy
+	 * @return strategy with the associated name or null on none found
 	 */
 	@Override
-	public void displayResults(String filename) throws IOException, ParseException{
-		File file = new File(filename);
-		Scanner fileReader = new Scanner(file);
-		while((fileReader.hasNextLine())){
-			String strategyName = fileReader.nextLine();
-			String symbol = fileReader.nextLine();
-			String dateString = fileReader.nextLine();
-			List<String[]> actionList = new ArrayList<String[]>();
-			List<BigDecimal> balanceList = new ArrayList<BigDecimal>();
-			List<Integer> holdingList = new ArrayList<Integer>();
-			//print result header
-			DateFormat format = new SimpleDateFormat("yyyy-mm-dd", Locale.ENGLISH);
-			Calendar cal=Calendar.getInstance();
-			cal.setTime((Date) format.parse(dateString));
-			dateString = format.format(cal.getTime());
-			System.out.println("Strategy: "+strategyName+" | Stock: "+symbol+" | Starting date: "+dateString);
-	
-			String line = null;
-			while(fileReader.hasNextLine()){
-				line = fileReader.nextLine();
-				if(line.equals("") || line == null){
-					break;
-				}
-					String[] dailyData=line.split(",");
-					balanceList.add(new BigDecimal(dailyData[0]));
-					holdingList.add(Integer.parseInt(dailyData[1]));
-					String[] actions = new String[dailyData.length-2];
-					System.arraycopy(dailyData,2, actions, 0, dailyData.length-2);
-					actionList.add(actions);
-					
-					if(actions.length!=0){
-						System.out.print(dateString);
-						System.out.print(" action: ");
-						System.out.print(actions[0]);
-						for(int i = 1; i < actions.length; i++){
-							System.out.print(", " + actions[i]);
-						}
-						System.out.println("");
-					}else{
-						System.out.println(dateString);
-					}
-					//increment date by 1
-					cal.setTime((Date) format.parse(dateString));
-					cal.add(Calendar.DATE, 1);
-					dateString=format.format(cal.getTime());
-			}
-			BigDecimal startingCapital = balanceList.get(0).add(new BigDecimal(holdingList.get(0)));
-			BigDecimal closingBalance = balanceList.get(balanceList.size()-1).add(new BigDecimal(holdingList.get(balanceList.size()-1)));
-			BigDecimal earnings = closingBalance.subtract((startingCapital));
-			BigDecimal percentEarning = earnings.multiply(new BigDecimal(100)).divide(startingCapital, 2, RoundingMode.HALF_UP);
-			System.out.println("Starting Capital: "+startingCapital);
-			System.out.println("Closing Balance: "+closingBalance);
-			System.out.println("Earnings: "+earnings);
-			System.out.println("Percent Earning: " + percentEarning + "%");
-			System.out.println("------------------------------------------------------------");
-			if(line==null){
-				break;
-			}
-		}
-		fileReader.close();
-	}
-	
-	/* (non-Javadoc)
-	 * @see autotradingsim.application.ITradingApplication#getStrategy(java.lang.String)
-	 */
-	@Override
-	public IStrategy getStrategy(String stratname){
+	public IStrategy getStrategy(String stratName){
 		return null;
 	}
-	/* (non-Javadoc)
-	 * @see autotradingsim.application.ITradingApplication#getStrategy(int)
-	 */
-	@Override
-	public IStrategy getStrategy(int stratid){
-		return strategies.get(stratid);	
-	}
 	
-	/* (non-Javadoc)
-	 * @see autotradingsim.application.ITradingApplication#saveStrategy(autotradingsim.strategy.IStrategy)
-	 */
-	@Override
-	public boolean saveStrategy (IStrategy newstrat){
-		if(strategies.containsKey(newstrat.hashCode())){
-			return false;
-		}else{
-		strategies.put(newstrat.getName().hashCode(), newstrat);
-			return true;
-		}
-	}
-	
+
 	/**
 	 * Loads a Stock to memory.
+	 * 
 	 * @param symbol: String representing the stock symbol to be loaded.
 	 */
 	private void loadStock(String symbol){
-		if (this.existsStock(symbol)){
+		if (this.stockExists(symbol)){
 			stocks.put(symbol, this.loader.fetchStock(symbol));
 		}
 	}
-	
-	/* (non-Javadoc)
-	 * @see autotradingsim.application.ITradingApplication#getStock(java.lang.String)
+
+	/**
+	 * Retrieve a Stock from application.
+	 *
+	 * @param symbol: official name of the stock
+	 * @return Stock object associated with stock
 	 */
 	@Override
 	public IStock getStock(String symbol) {
@@ -176,26 +129,26 @@ public class TradingApplication implements ITradingApplication {
 		}
     }
 	
-	/* (non-Javadoc)
-	 * @see autotradingsim.application.ITradingApplication#existsStock(java.lang.String)
+
+	/**
+	 * Check if a Stock symbol is available in data.
+	 * 
+	 * @param symbol: String of a stock symbol, not case sensitive.
+	 * @return true if stock exists in data, false otherwise.
 	 */
 	@Override
-	public boolean existsStock(String symbol) {
+	public boolean stockExists(String symbol) {
         return loader.exists(symbol);
     }
 	
-	/* (non-Javadoc)
-	 * @see autotradingsim.application.ITradingApplication#getStockSymbols()
+
+	/**
+	 * Get an iterator of loaded stock symbols.
+	 * 
+	 * @return Iterator<String> of stock symbols available.
 	 */
 	@Override
 	public Iterator<String> getStockSymbols(){
-		// TODO Change this so we can get a list of all possible stocks.
 		return this.stocks.keySet().iterator();
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
 	}
 }
