6a8159908da1bdbefa518bc56b19d409f1cade32
Bill Feng <bill.zijing.feng@gmail.com>
2015-11-13 18:37:57 -0500

    Creating GUI

---
+++ automated-trading-simulator/src/main/java/autotradingsim/Interface/AutomatdTradingSimulator.java
@@ -0,0 +1,14 @@
+package autotradingsim.Interface;
+
+import javax.swing.*;
+
+/**
+ * Created by billfeng on 15-11-13.
+ */
+public class AutomatdTradingSimulator {
+    private JList experiments;
+    private JButton add;
+    private JButton open;
+    private JButton delete;
+    private JButton exit;
+}
