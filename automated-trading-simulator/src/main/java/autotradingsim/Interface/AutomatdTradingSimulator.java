183855d0c15d5ece29272208f197b45c1890a149
Bill Feng <bill.zijing.feng@gmail.com>
2015-11-13 19:33:51 -0500

    Adding Experiment UI

--- automated-trading-simulator/src/main/java/autotradingsim/Interface/AutomatdTradingSimulator.java
+++ automated-trading-simulator/src/main/java/autotradingsim/Interface/AutomatdTradingSimulator.java
@@ -1,9 +1,12 @@
 package autotradingsim.Interface;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by billfeng on 15-11-13.
+ *
  */
 public class AutomatdTradingSimulator {
     private JList experiments;
@@ -11,4 +14,14 @@ public class AutomatdTradingSimulator {
     private JButton open;
     private JButton delete;
     private JButton exit;
+
+    public AutomatdTradingSimulator() {
+        exit.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                System.exit(0);
+            }
+        });
+    }
 }
