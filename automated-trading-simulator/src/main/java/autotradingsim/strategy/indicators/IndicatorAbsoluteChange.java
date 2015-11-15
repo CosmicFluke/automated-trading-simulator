078fedc0c45bcc952233722c286097c0cacee907
Asher Minden-Webb <CosmicFluke@users.noreply.github.com>
2015-11-15 15:54:01 -0500

    Renamed IndicatorChange to IndicatorAbsoluteChange.  Updated javadocs.  Cleaned up imports.

--- automated-trading-simulator/src/main/java/autotradingsim/strategy/indicators/IndicatorAbsoluteChange.java
+++ automated-trading-simulator/src/main/java/autotradingsim/strategy/indicators/IndicatorAbsoluteChange.java
@@ -1,30 +1,26 @@
 package autotradingsim.strategy.indicators;
 
-import autotradingsim.stocks.StockDay;
-import autotradingsim.strategy.BufferAdapter;
 import autotradingsim.strategy.IBufferAdapter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Iterator;
 import java.util.function.Function;
-import java.util.stream.Stream;
 
 /**
  * Created by Asher on 2015-11-15.
  */
-public class IndicatorChange extends MetaIndicator {
+public class IndicatorAbsoluteChange extends MetaIndicator {
 
-    static String default_name = "Indicator Trend";
+    static String default_name = "Absolute Change Meta-Indicator";
     static String default_description =
-            "This Indicator shows a change in another indicator, such as a Moving Average, over some specified number of days.";
+            "This Indicator shows an absolute change in another indicator, such as a Moving Average, over some specified number of days.";
 
     /**
-     * Construct a new IndicatorChange that follows the change in the given indicator over a period of numDays.
+     * Construct a new IndicatorAbsoluteChange that follows the change in the given indicator over a period of numDays.
      * @param indicator an Indicator to follow
      * @param numDays Number of days to average for a change in Indicator's value
      */
-    public IndicatorChange(Indicator indicator, int numDays) {
+    public IndicatorAbsoluteChange(Indicator indicator, int numDays) {
         super(indicator, numDays, default_name, default_description);
     }
 
