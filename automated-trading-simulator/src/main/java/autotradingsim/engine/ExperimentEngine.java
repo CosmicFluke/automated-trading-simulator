3b6b31ecfbaee24bdd7cfff41e9fdcd9988aea3a
xshirl1027 <xshirl1027@gmail.com>
2015-11-18 18:09:52 -0500

    UI stores experiment

--- automated-trading-simulator/src/main/java/autotradingsim/engine/ExperimentEngine.java
+++ automated-trading-simulator/src/main/java/autotradingsim/engine/ExperimentEngine.java
@@ -35,7 +35,7 @@ public class ExperimentEngine {
 	 * @return retExp 
 	 * takes an experiment name, returns the new experiment created
 	 */
-	protected static Experiment createExperiment(String expname){
+	public Experiment createExperiment(String expname){
 		Experiment retExp=null;
 		if(appEngine.setExperiment(expname, new Experiment(expname))){
 			retExp=(Experiment) appEngine.getExperiment(expname);
@@ -43,27 +43,27 @@ public class ExperimentEngine {
 		return retExp;
 	}
 	/**
-	 * 
+	 *
 	 * @param expname
 	 * @return Experiment
 	 * takes experiment name and returns experiment object with that name
 	 */
-	protected static Experiment viewExperiment(String expname) {
+	public Experiment getExperiment(String expname) {
 		// TODO Auto-generated method stub
 		return (Experiment) appEngine.getExperiment(expname);
 	}
 	/**
-	 * 
+	 *
 	 * @param expName
 	 * @param stratname
 	 * take experiment name, adds the strategy name to it's list of strategy
 	 * @return boolean
 	 */
-	protected static boolean addStrategy(String expName, String stratname){
+	public boolean addStrategy(String expName, String stratname){
 		return(appEngine.getExperiment(expName).addStrategy(stratname));
 	}
 
-	public static void addtimeset(String currentExperiment, String string, String string2) {
+	public void addtimeset(String currentExperiment, String string, String string2) {
 		// TODO Auto-generated method stub
 		
 	}
