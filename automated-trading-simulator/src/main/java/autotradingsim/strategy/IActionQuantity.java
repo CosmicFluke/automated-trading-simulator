a221f9e338960049c69dadd2f25292c8f6b71c0a
Asher Minden-Webb <CosmicFluke@users.noreply.github.com>
2015-11-22 08:07:53 -0500

    Made IActionQuantity a functional interface with three parameters. - Revised ConstantActionQuantity to match new interface.  Added javadocs. - Added new VariableBalanceActionQuantity.  Likely not needed, but could be useful (see javadocs)

--- automated-trading-simulator/src/main/java/autotradingsim/strategy/IActionQuantity.java
+++ automated-trading-simulator/src/main/java/autotradingsim/strategy/IActionQuantity.java
@@ -5,15 +5,18 @@ import java.math.BigDecimal;
 /**
  * Created by Asher on 2015-10-30.</br>
  *
- * <p>This is a machine which can be used to calculate a quantity for an action.</p>
+ * <p>This is a machine (function) which can be used to calculate a quantity for an action.</p>
+ * <p>Functional interface takes three parameters: <br>
+ *     {@link BigDecimal} balance<br>
+ *     {@link BigDecimal} stockValue<br>
+ *     {@link ConfidenceFactor} confidence</p>
  *
  */
+@FunctionalInterface
 public interface IActionQuantity {
 
-    default int getValue() {
-        return 0;
-    }
+    enum ConfidenceFactor { LOW, MEDIUM, HIGH }
 
-    int getValue(BigDecimal balance);
+    int getValue(BigDecimal balance, BigDecimal stockValue, ConfidenceFactor confidence);
 
 }
