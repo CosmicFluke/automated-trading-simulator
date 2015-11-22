37428954062fa39e3668b28f8fa448a6ada28ef9
Asher Minden-Webb <CosmicFluke@users.noreply.github.com>
2015-11-22 15:11:48 -0500

    Implemented new method "newAdvancedStrategy" for StrategyDemoFactory Also modified "newBasicStrategy" method to take in parameters for buy/sell price thresholds

--- automated-trading-simulator/src/main/java/autotradingsim/util/StrategyDemoFactory.java
+++ automated-trading-simulator/src/main/java/autotradingsim/util/StrategyDemoFactory.java
@@ -1,6 +1,8 @@
 package autotradingsim.util;
 
 import autotradingsim.strategy.*;
+import autotradingsim.strategy.indicators.IndicatorAbsoluteChange;
+import autotradingsim.strategy.indicators.IndicatorRelativeChange;
 import autotradingsim.strategy.indicators.SimpleMovingAverage;
 import autotradingsim.strategy.rules.*;
 
@@ -18,12 +20,13 @@ public class StrategyDemoFactory {
      * autotradingsim.deprecated.simpleimpl.SimpleStockValue})</li>
      *     <li>IActions are {@link Action} constructed with constant <tt>quantity == 10</tt></li>
      *     <li>The rule with the {@link IAction.ActionType#BUY} action will buy when stock
-     *     value <= 100</li>
+     *     value <= buyBelow</li>
      *     <li>The rule with the {@link IAction.ActionType#SELL} action will sell when stock
-     *     value >= 150</li>
+     *     value >= sellAbove</li>
      * </ul></p>
+     *
      */
-    public static IStrategy newBasicStrategy() {
+    public static IStrategy newBasicStrategy(BigDecimal buyBelow, BigDecimal sellAbove, int buyNum, int sellNum) {
         // Create new empty strategy
         IStrategy strat = new Strategy("Basic strategy",
                 "A basic example of a strategy, that buys 10 units of stock whenever the price is under $100 and " +
@@ -35,21 +38,21 @@ public class StrategyDemoFactory {
         rule.addCondition(new StaticCondition(
                 new SimpleMovingAverage(1),
                 ICondition.Comparator.LEQ,
-                BigDecimal.valueOf(100)));
+                buyBelow));
         // Specify action using constant quantity
-        rule.addAction(new Action(IAction.ActionType.BUY, 10));
+        rule.addAction(new Action(IAction.ActionType.BUY, buyNum));
         // Add to strategy
         strat.addRule(rule);
 
         // Create new rule (for selling)
         rule = new Rule("Sell 10 @ over 150",
                 "A basic example of a rule, that sells 10 units of stock whenever the price is at or over $150");
-        rule.addCondition(new StaticCondition(
+        rule.addCondition(new StaticCondition(          // Stock value is greater than 150
                 new SimpleMovingAverage(1),
                 ICondition.Comparator.GEQ,
-                BigDecimal.valueOf(150)));
+                sellAbove));
         // Specify action using constant quantity
-        rule.addAction(new Action(IAction.ActionType.SELL, 10));
+        rule.addAction(new Action(IAction.ActionType.SELL, sellNum));
         // Add to strategy
         strat.addRule(rule);
 
@@ -57,11 +60,39 @@ public class StrategyDemoFactory {
     }
 
     /**
-     * TODO: Not yet implemented
+     *
      * @return new strategy
      */
     public static IStrategy newAdvancedStrategy() {
-        IStrategy strat = new Strategy("");
+        IStrategy strat = new Strategy("Advanced Strategy 1",
+                "An example of a strategy with two rules and non-constant values in Actions and Indicators.");
+
+        IRule rule = new Rule("Buy increasing stock, short-term",
+                "Buy half of my balance worth of a stock if its 5-day moving average increases by 2% over 3 days.");
+        rule.addCondition(new StaticCondition(      // 5-day moving average increases by 2% over 3 days
+                new IndicatorRelativeChange(new SimpleMovingAverage(5), 3),
+                ICondition.Comparator.GEQ,
+                BigDecimal.valueOf(0.02)));
+        rule.addAction(new Action(
+                IAction.ActionType.BUY,
+                new VariableBalanceActionQuantity(
+                        (num_stocks) -> num_stocks
+                                .divide(BigDecimal.valueOf(2))
+                                .intValue())));
+        rule = new Rule("Sell decreasing stock, long term",
+                "");
+        rule.addCondition(new StaticCondition(      // 30-day moving average decreases by 2% over 3 days
+                new IndicatorRelativeChange(new SimpleMovingAverage(30), 5),
+                ICondition.Comparator.LEQ,
+                BigDecimal.valueOf(-0.02)));
+        rule.addCondition(new StaticCondition(      // 5-day simple moving average dropped by at least $2.00 in 2 days
+                new IndicatorAbsoluteChange(new SimpleMovingAverage(5), 2),
+                ICondition.Comparator.GEQ,
+                BigDecimal.valueOf(2)));
+        rule.addAction(new Action(                  // SELL SELL SELL (up to 1000 shares, for now)
+                IAction.ActionType.SELL,
+                new ConstantActionQuantity(1000))); // TODO: Needs to be fixed once shares-based quantities are implemented
+
         return strat;
     }
 
