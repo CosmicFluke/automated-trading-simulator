ac9233853c2e174024142d1baa269c24afdbef63
Tomek-Stolarczyk <tomek.stolarczyk9@gmail.com>
2015-11-21 20:14:16 -0500

    modify package name to reflect changes to Terminal tests

--- automated-trading-simulator/src/test/java/autotradingsim/terminal/TerminalTestTODO.java
+++ automated-trading-simulator/src/test/java/autotradingsim/terminal/TerminalTestTODO.java
@@ -17,7 +17,7 @@ import autotradingsim.autoTradingSim;
 /**
  * Unit test for simple AppPlaceholder.
  */
-public class TerminalTest
+public class TerminalTestTODO
     extends TestCase
 {
     /**
@@ -25,7 +25,7 @@ public class TerminalTest
      *
      * @param testName name of the test case
      */
-    public TerminalTest( String testName )
+    public TerminalTestTODO( String testName )
     {
         super( testName );
     }
@@ -35,7 +35,7 @@ public class TerminalTest
      */
     public static Test suite()
     {
-        return new TestSuite( TerminalTest.class );
+        return new TestSuite( TerminalTestTODO.class );
     }
 
     
