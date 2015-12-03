20aa48a0da9656f9ea9abb479c7604bea3e303d3
Asher Minden-Webb <asher.minden.webb@mail.utoronto.ca>
2015-12-02 21:09:03 -0500

    Added CalcMode to to IndicatorAbsoluteChange and begin implementing alternate implementation for average change vs net change.

--- automated-trading-simulator/src/main/java/autotradingsim/strategy/indicators/IndicatorAbsoluteChange.java
+++ automated-trading-simulator/src/main/java/autotradingsim/strategy/indicators/IndicatorAbsoluteChange.java
@@ -5,7 +5,10 @@ import autotradingsim.strategy.rules.IMeasurement;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 /**
  * Created by Asher on 2015-11-15.
@@ -19,17 +22,42 @@ public class IndicatorAbsoluteChange extends MetaIndicator {
                     "specified number of days.";
 
     /**
+     * <p>Enumerates the calculation mode for this MetaIndicator.</p>
+     * <p>DAILY_AVERAGE_CHANGE averages the daily changes in the indicator over numDays</p>
+     * <p>NET_CHANGE calculates the net change from the numDays before the current date to the current date, ignoring
+     * values in between</p>
+     */
+    enum CalcMode {
+        /**
+         * Calculation mode that averages the daily changes in the indicator over numDays
+         */
+        DAILY_AVERAGE_CHANGE,
+        /**
+         * Calculation mode that calculates the net change from the numDays before the current date to the current date,
+         * ignoring values in between
+         */
+        NET_CHANGE
+    }
+
+    private CalcMode mode;
+
+    /**
      * Construct a new IndicatorAbsoluteChange that follows the change in the given indicator over a period of numDays.
      * @param indicator an Indicator to follow
-     * @param numDays Number of days to average for a change in Indicator's value
+     * @param numDays Number of days to average for a change in Indicator's value.  Must be at least 1.
      */
     public IndicatorAbsoluteChange(IMeasurement indicator, int numDays) {
+        this(indicator, numDays, true);
+    }
+
+    public IndicatorAbsoluteChange(IMeasurement indicator, int numDays, CalcMode calcMode) {
         super(indicator, numDays, default_name, default_description);
+        mode = calcMode;
     }
 
     public BigDecimal[] getFirstAndSecond(IBufferAdapter adapter) {
         LocalDate lastDate = adapter.getLastDay();
-        LocalDate firstDate = lastDate.minusDays(numDays);
+        LocalDate firstDate = lastDate.minusDays(numDays - 1);
         adapter.updateTo(firstDate);
         BigDecimal firstVal = indicator.getValue(adapter);
         adapter.updateTo(lastDate);
@@ -38,9 +66,17 @@ public class IndicatorAbsoluteChange extends MetaIndicator {
         return new BigDecimal[]{firstVal, secondVal};
     }
 
+    public List<BigDecimal> getAllDays(IBufferAdapter adapter) {
+        List<BigDecimal> allDays = new ArrayList<>();
+        for (int i = 0; i < numDays; i++) {
+
+        }
+    }
+
     @Override
     public BigDecimal getValue(IBufferAdapter adapter) {
         BigDecimal[] firstAndSecond = getFirstAndSecond(adapter);
+
         return firstAndSecond[1].subtract(firstAndSecond[0]);
     }
 
