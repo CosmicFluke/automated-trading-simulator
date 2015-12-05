b93f5688bcf28b97f89f94d3e310848ef2ae881c
Asher Minden-Webb <amindenwebb@gmail.com>
2015-12-05 15:26:44 -0500

    Updated advanced prefab strategy in StrategyDemoFactory  - Also improved IActionQuantity javadocs  - Minor modification to designdocument.md

--- automated-trading-simulator/src/main/java/autotradingsim/strategy/rules/IActionQuantity.java
+++ automated-trading-simulator/src/main/java/autotradingsim/strategy/rules/IActionQuantity.java
@@ -7,15 +7,25 @@ import java.math.BigDecimal;
  * Created by Asher on 2015-10-30.</br>
  *
  * <p>This is a machine (function) which can be used to calculate a quantity for an action.</p>
- * <p>Functional interface takes three parameters: <br>
- *     {@link BigDecimal} balance<br>
+ * <p>Functional method takes four parameters: <br>
+ *     {@link BigDecimal} cashBalance<br>
  *     {@link BigDecimal} stockValue<br>
+ *     <b>int</b> numSharesOwned<br>
  *     {@link ConfidenceFactor} confidence</p>
+ *  <p>Has one method, {@link IActionQuantity#getValue(BigDecimal, BigDecimal, int, ConfidenceFactor)}</p>
  *
  */
 @FunctionalInterface
 public interface IActionQuantity extends Serializable {
 
-    int getValue(BigDecimal balance, BigDecimal stockValue, int numSharesOwned, ConfidenceFactor confidence);
+    /**
+     *
+     * @param cashBalance current cash balance
+     * @param stockValue value of the stock to be bought/sold
+     * @param numSharesOwned number of shares of this stock currently owned
+     * @param confidence a confidence factor
+     * @return
+     */
+    int getValue(BigDecimal cashBalance, BigDecimal stockValue, int numSharesOwned, ConfidenceFactor confidence);
 
 }
