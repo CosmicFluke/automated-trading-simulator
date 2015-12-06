91a51f8e09633d1b72059a3cb8e1902a351a013a
Asher Minden-Webb <amindenwebb@gmail.com>
2015-12-06 18:57:42 -0500

    TemporaryTestingStrategies: made application a local variable (instead of static variable)

--- automated-trading-simulator/src/main/java/autotradingsim/util/TemporaryTestingStrategies.java
+++ automated-trading-simulator/src/main/java/autotradingsim/util/TemporaryTestingStrategies.java
@@ -9,9 +9,10 @@ import java.math.BigDecimal;
  * Created by Shirley on 2015-12-05.
  */
 public class TemporaryTestingStrategies {
-    static TradingApplication application = TradingApplication.getInstance();
+
     public static void createStrats(){
-        application.setStrategy(StrategyDemoFactory.newBasicStrategy(new BigDecimal(10), new BigDecimal(20), 10,10));
+        ITradingApplication application = TradingApplication.getInstance();
+        application.setStrategy(StrategyDemoFactory.newBasicStrategy(new BigDecimal(10), new BigDecimal(20), 50,25));
         application.setStrategy(StrategyDemoFactory.newAdvancedTestingStrategy());
         application.setStrategy(StrategyDemoFactory.newAdvancedStrategy());
     }
